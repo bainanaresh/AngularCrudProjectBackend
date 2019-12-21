6f1358f57de74125150d0a5259babd70400ba581
Baina Naresh <bainanaresh@gmail.com>
2019-12-22 00:58:15 +0530

    removed get index method from controller class

--- src/main/java/com/angular/controller/AngularController.java
+++ src/main/java/com/angular/controller/AngularController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.angular.entity.User;
@@ -22,11 +21,6 @@ public class AngularController {
 	
 	@Autowired
 	private UserService service;
-	
-	@RequestMapping("/index")
-	public String getIndex(){
-	return "index";	
-	}
 	
 	@GetMapping("/users")
 	public List<User> getAllUsers(){
